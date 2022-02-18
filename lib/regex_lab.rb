b5d62b37f965b5c6c9634055f1ae65fb885d6037
ajlic24@gmail.com <ajlic24@gmail.com>
2022-02-18 06:24:15 -0500

    commit tree c4536f0e63af13933acae292c1d96d6999caf1fd

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,23 @@
-def starts_with_a_vowel?(word)
+require 'pry'
 
+def starts_with_a_vowel?(word)
+    !!word.match(/^[aeiouAEIOU]/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+    text.split.grep /^un\w+ing$/
 end
 
 def words_five_letters_long(text)
-
+    text.split.grep /^\w{5}$/
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+    !!text.match(/^[A-Z].+[.?!]$/)
 end
 
 def valid_phone_number?(phone)
-
+    phone.scan(/[0-9]/).length == 10
 end
+
+# puts first_word_capitalized_and_ends_with_punctuation? "i wondered why the baseball was getting bigger. Then it hit me."
